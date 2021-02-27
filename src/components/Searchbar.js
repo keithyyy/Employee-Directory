421d04008939a5d8058c3a79846dbb7cc04d0b35
Keith Crooc <keith.crooc@gmail.com>
2021-02-27 15:26:09 -0500

    added search bar component

---
+++ src/components/Searchbar.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const SearchBar = (props) => {
+    return (
+        <div className="col-9 form-control mb-2 searchbar">
+            <input type="text" 
+            value={props.value} 
+            onChange={props.handleInputChange}
+            placeholder="Find an Employee"/>
+
+
+            <div className="btn btn-success col-3 ml-3" onClick={props.changeButtonText}>Search</div>
+        
+        </div>
+
+        
+    )
+}
+
+export default SearchBar;
--- src/components/header.js
+++ src/components/header.js
@@ -8,15 +8,7 @@ const Header = () => {
                 <div className="col-12">
                     <h3>Employee Directory</h3>
                 </div>            
-            </div>
-
-            <div className="row">
-                <div className="col-12">
-                    <h5>Search through an your employee database</h5>
-                </div>
-            </div>
-            
-            
+            </div>   
         </header>
     )
 }
