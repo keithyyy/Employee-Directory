c95cddec570406d878dd361b8f320aa71650b54e
Keith Crooc <keith.crooc@gmail.com>
2021-02-27 16:08:01 -0500

    npm install axios and added random user api

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,8 +2,11 @@ import React from "react";
 import SearchBar from "./Searchbar"
 import TableHead from "./TableHead"
 import TableItems from "./TableItems"
+import API from "../utils/API"
 
 const Container = () => {
+
+
     return (
         <div className="container mt-4">
             <p className="text-center">Search through your employee database!</p>
@@ -24,10 +27,6 @@ const Container = () => {
 
         </div>
     )
-
-
-
-
 
 }
 
