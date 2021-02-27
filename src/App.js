3386a2522671f32d41346d6a9a256d43f143d73a
Keith Crooc <keith.crooc@gmail.com>
2021-02-26 23:02:54 -0500

    removed extra folder

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+     <h1>Hey Keith</h1>
+     <h2>It's me from the past</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <headher>
+            <h1>Hello</h1>
+        </headher>
+    )
+}
+
+export default Header
