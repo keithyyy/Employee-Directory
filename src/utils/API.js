e6773a7850e0f1e9fe964231c87495439325a3ce
Keith Crooc <keith.crooc@gmail.com>
2021-02-27 18:24:00 -0500

    added debounce to handle filtering

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default {
     getUsers: function() {
-        return axios.get('https://randomuser.me/api/?results=10&nat=us&seed=foobar&inc=picture,name,phone,email,dob')        
+        return axios.get('https://randomuser.me/api/?results=10&nat=us&seed=foobar&inc=picture,name,phone,email,dob,id')        
     }
 }
